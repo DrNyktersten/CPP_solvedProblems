b10bc24543aa695efe56417d7cd380393e7f20cb
Kurmanbek Bazarov <93934476+DrNyktersten@users.noreply.github.com>
2022-05-17 22:06:49 -0500

    Update Falling_Distance.cpp

--- chapter 6/Falling_Distance.cpp
+++ chapter 6/Falling_Distance.cpp
@@ -1,4 +1,16 @@
+/*
+Problem
+When an object is falling because of gravity, the following formula can be used to determine the distance the object falls in a specific time period:
 
+d = 1/2gt^2
+
+The variables in the formula are as follows: d is the distance in meters, g is 9.8, and t is the amount of time, in seconds, that the object has been falling.
+
+Write a function named fallingDistance that accepts an object’s falling time (in seconds) as an argument.
+The function should return the distance, in meters, that the object has fallen during that time interval. 
+Write a program that demonstrates the function by calling it in a loop that passes
+the values 1 through 10 as arguments and displays the return value.
+*/
 #include<iostream> 
 #include<cmath>
 using namespace std;
@@ -7,7 +19,7 @@ const double g = 9.8;
 double fallingDistance(double); 
 
 //formula of distance: 
-// d = 1/2gt^2 
+//d = 1/2gt^2 
 int main() 
 {   
     double t, 
